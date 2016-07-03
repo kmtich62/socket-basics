819715d8310d4b4a37a76b9245a9e9e224381c97
Kevin Mitchell <kmitchell62@gmail.com>
2016-07-03 16:17:02 -0400

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('Connected to socket.io server!');
-})
+});
+
+socket.on('message', function(message){
+    console.log('New message recieved:');
+    console.log(message.text);
+});
