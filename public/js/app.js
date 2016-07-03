be1269663674ecac441e430ffedb3a289f741043
Kevin Mitchell <kmitchell62@gmail.com>
2016-07-03 16:44:22 -0400

    added input box to web form

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,14 @@ socket.on('message', function(message){
     console.log('New message recieved:');
     console.log(message.text);
 });
+
+//handles submitting of new messages
+
+
+var $form = jQuery('#message-Form');
+$form.on('submit', function(event){
+    var $inputBox = $form.find('input[name=message]');
+    event.preventDefault();
+    socket.emit('message', {text: $inputBox.val()});
+    $inputBox.val('').focus();
+});
