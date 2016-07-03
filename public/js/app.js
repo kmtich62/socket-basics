b759513fcea78b1caa368b2c5ba029415eacd418
Kevin Mitchell <kmitchell62@gmail.com>
2016-07-03 16:57:40 -0400

    added display message div to UI page

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ socket.on('connect', function(){
 socket.on('message', function(message){
     console.log('New message recieved:');
     console.log(message.text);
+
+    jQuery('.messages').append('<p>'+ message.text +'</p>')
+
 });
 
 //handles submitting of new messages
